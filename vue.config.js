c175cb8c055b44fa8d4d88902086887e6de0d4fa
onghdzaa <gamezzbbzz@gmail.com>
2024-05-06 00:39:52 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = {
+//   devServer: {
+//     port: 8085, // your desired port number
+//     proxy: 'http://pm2.derive.co.th/',
+//   },
+// };
 module.exports = {
-  devServer: {
-    port: 8085, // your desired port number
-    proxy: 'http://pm2.derive.co.th/',
-  },
-};
+  pwa: {
+      workboxOptions: {
+        exclude: [/_redirects/]
+      }
+    }
+}
 // module.exports = {
 //   publicPath: process.env.NODE_ENV === 'production'
 //     ? '/ThailandAccessibleTourism/'
